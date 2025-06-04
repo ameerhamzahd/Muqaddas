094ab66a1b5ce4bd5b3490d97b18aad0200e7b89
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-06-05 00:38:05 +0600

    Muqaddas MainLayout-Functionality done.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Outlet, useNavigation } from 'react-router';
+import ScrollToTop from '../components/Shared/ScrollToTop/ScrollToTop';
+import Navbar from '../components/Shared/Navbar/Navbar';
+import Loader from '../components/Shared/Loader/Loader';
+import Footer from '../components/Shared/Footer/Footer';
 
 const MainLayout = () => {
+
+    const {state} = useNavigation();
+
     return (
         <div>
-            
+            <ScrollToTop></ScrollToTop>
+
+            <header>
+                <Navbar></Navbar>
+            </header>
+
+            <main>
+                {
+                    state === "loading" ? <Loader></Loader> : <Outlet></Outlet>
+                }
+            </main>
+
+            <footer>
+                <Footer></Footer>
+            </footer>
         </div>
     );
 };

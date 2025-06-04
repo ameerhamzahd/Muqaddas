9d6400411c65bc6a972640b82306508c8b63c993
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-06-05 03:51:05 +0600

    Muqaddas Loader-UI done.

--- src/components/Shared/Loader/Loader.jsx
+++ src/components/Shared/Loader/Loader.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Loader = () => {
     return (
         <div>
-            
+            <div className='flex justify-center items-center min-h-screen'>
+                <span className="text-secondary loading loading-spinner loading-xl"></span>
+            </div>
         </div>
     );
 };
